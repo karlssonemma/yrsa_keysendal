5e24d4e69d3f7302da004910baf8c2f358c68258
emma <karlssonemma93@gmail.com>
2021-09-14 22:05:56 +0200

    nav horizontal on mobile and vertical on desktop

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,8 +3,22 @@ import styled from 'styled-components';
 import { Link } from 'gatsby';
 import theme from '../utils/theme';
 
+const Navigation = styled.nav`
+    width: 100%;
+`;
+
 const List = styled.ul`
+    padding: 0;
     list-style: none;
+    background-color: blue;
+    display: flex;
+
+    @media screen and (min-width: 800px) {
+        background-color: green;
+        flex-direction: column;
+        position: fixed;
+        height: 100vh;
+    }
 `;
 
 const StyledLink = styled(Link)`
@@ -26,7 +40,7 @@ const StyledLink = styled(Link)`
     &:hover {
         font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
     }
-`
+`;
 
 const Layout = ({ children }) => {
 
